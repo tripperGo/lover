379111f07fc10c5341f4159c95b2aead5b499e7c
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2019-06-04 17:37:58 +0800

    第一版完成

--- src/main/resources/static/js/something.js
+++ src/main/resources/static/js/something.js
@@ -1,3 +1,5 @@
+var arrData = [], timerID = 0;
+
 (function ($) {
     initSomething();
 })(jQuery);
@@ -9,23 +11,61 @@ function initSomething() {
         if (keyStr) {
             var key = keyStr.replace("key=", "");
             // if (key) {
-                $.ajax({
-                    url: "something/list/" + key,
-                    data: {},
-                    type: "GET",
-                    dataType: "JSON",
-                    success: function (data) {
-                        for(var index in data){
-                            var item = data[index];
-                            var itemHtml = "<a href=\"#\" class=\"list-group-item\">\n" +
-                                item.title +
-                                "                <span class=\"badge\">"+item.goOver+"</span>\n" +
-                                "            </a>";
-                            $(".something-parent").append(itemHtml)
-                        }
+            $.ajax({
+                url: "something/list/" + key,
+                data: {},
+                type: "GET",
+                dataType: "JSON",
+                success: function (data) {
+                    for (var index in data) {
+                        var item = data[index];
+                        arrData.push(item.title)
+                        var itemHtml = "<a href=\"#\" class=\"list-group-item\">\n" +
+                            item.title +
+                            "                <span class=\"badge\">" + item.goOver + "</span>\n" +
+                            "            </a>";
+                        $(".something-parent").append(itemHtml)
                     }
-                });
+                }
+            });
             // }
         }
     }
+}
+
+function getEleById(id) {
+    return document.getElementById(id);
+}
+
+function Start(btn) {
+    if (timerID > 0) {
+        clearInterval(timerID);
+        timerID = 0;
+    }
+    if (arrData.length < 1) {
+        showAlertSomething("都随机完一遍啦")
+        return;
+    }
+    if (btn.value == "开始") {
+        timerID = setInterval(function () {
+            getEleById("num").index = parseInt(Math.random() * (arrData.length));
+            getEleById("num").innerHTML = arrData[getEleById("num").index];
+        }, 30);
+        btn.value = "暂停";
+    } else {
+        var div = document.createElement("div");
+        div.innerHTML = getEleById("num").innerHTML;
+        getEleById("result").appendChild(div);
+        arrData.splice(getEleById("num").index, 1);
+        btn.value = "开始";
+    }
+}
+
+function showAlertSomething(msg) {
+    $(".lot-div-main").append("<div class=\"alert alert-danger alert-danger-msg\" role=\"alert\">" + msg + "</div>");
+    setInterval(hiddenAlertSomething, 3000);
+}
+
+function hiddenAlertSomething() {
+    $(".lot-div-main .alert-danger").remove();
 }
