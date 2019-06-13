389bf5195a31316d8fcf9babec93c2a042931a14
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2019-06-13 11:50:13 +0800

    淡入淡出

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,9 @@
 (function ($) {
     // 加载区块数据
     initPrefecture();
+
+    // 加载活动计划数据
+    initPlanning();
 })(jQuery);
 
 function initPrefecture() {
@@ -14,11 +17,32 @@ function initPrefecture() {
                 var item = data[index];
                 var prefectureHtml = "<div class=\"col-xs-6 col-md-3\">\n" +
                     "                <a href=\"/something.html?key=" + item.key + "\" class=\"thumbnail\">\n" +
-                    "                    <img class='img-index-layout' src=\'" + item.appender + "\' alt=\"...\">\n" +
+                    "                    <img class='img-index-layout img-index-layout-shadow' src=\'" + item.appender + "\' alt=\"...\">\n" +
                     "                    <div>" + item.value + "</div>\n" +
                     "                </a>\n" +
                     "            </div>";
                 $(".card-prefecture-parent").append(prefectureHtml);
+            }
+        }
+    });
+}
+
+function initPlanning() {
+    $.ajax({
+        url: "index/dic/planning",
+        data: {},
+        type: "GET",
+        dataType: "JSON",
+        success: function (data) {
+            for (var index in data) {
+                var item = data[index];
+                var planningHtml = "<div class=\"col-xs-6 col-md-3\">\n" +
+                    "                <a href=\"/something.html?key=" + item.key + "\" class=\"thumbnail text-center\">\n" +
+                    "                    <img class='img-index-layout-shadow' src=\'" + item.appender + "\' alt=\"...\">\n" +
+                    "                    <div>" + item.value + "</div>\n" +
+                    "                </a>\n" +
+                    "            </div>";
+                $(".card-planning-parent").append(planningHtml);
             }
         }
     });
